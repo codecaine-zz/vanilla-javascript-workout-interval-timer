d19baf2f7b1433f64493b32177d1d81f1e901da2
Jerome Scott <codecaine@gmail.com>
2023-03-18 04:00:01 +0000

    abstracted javascript code more and added rest a required field

--- script.js
+++ script.js
@@ -24,155 +24,148 @@ clearWorkoutsBtn.addEventListener('click', clearWorkouts);
 startBtn.addEventListener('click', startTimer);
 pauseBtn.addEventListener('click', pauseTimer);
 
-
-// Functions
+// add workout all input fields must be filled out
 function addWorkout() {
-  const exerciseName = exerciseNameInput.value.trim();
-  const reps = parseInt(repsInput.value);
-  const sets = parseInt(setsInput.value);
-  const interval = parseInt(intervalInput.value);
-  const rest = parseInt(restInput.value); 
-  
-  if (exerciseName && reps && sets && interval) {
-    const workout = {
-      exerciseName,
-      reps,
-      sets,
-      interval,
-      rest 
-    };
+    const exerciseName = exerciseNameInput.value.trim();
+    const reps = parseInt(repsInput.value);
+    const sets = parseInt(setsInput.value);
+    const interval = parseInt(intervalInput.value);
+    const rest = parseInt(restInput.value);
 
-    workoutListData.push(workout);
-    displayWorkout(workout);
-    exerciseNameInput.value = '';
-  }
+    if (exerciseName && reps && sets && interval && rest) {
+        const workout = {
+            exerciseName,
+            reps,
+            sets,
+            interval,
+            rest
+        };
+
+        workoutListData.push(workout);
+        displayWorkout(workout);
+        exerciseNameInput.value = '';
+    }
 }
 
 function displayWorkout(workout) {
-  const li = document.createElement('li');
-li.textContent = `${workout.exerciseName} (${workout.reps} reps) x ${workout.sets} sets - ${workout.interval}s interval - ${workout.rest}s rest`;
-  li.dataset.completed = false;
-  workoutList.appendChild(li);
+    const li = document.createElement('li');
+    li.textContent = `${workout.exerciseName} (${workout.reps} reps) x ${workout.sets} sets - ${workout.interval}s interval - ${workout.rest}s rest`;
+    li.dataset.completed = false;
+    workoutList.appendChild(li);
 }
 
 function clearWorkouts() {
-  workoutList.innerHTML = '';
-  workoutListData = [];
-  currentWorkoutIndex = 0;
-  currentSet = 1;
-  clearInterval(timer); // Clear the interval timer
-  timerDisplay.textContent = '00:00'; // Reset the timer display
-  completionMessage.classList.add('d-none'); // Hide the completion message
-  document.getElementById('current-set').textContent = ''; // Clear the current set
-  currentExerciseDisplay.textContent = ''; // Clear the current exercise
-  startBtn.disabled = false; // Enable the start button
-  pauseBtn.disabled = true; // Disable the pause button
+    workoutList.innerHTML = '';
+    workoutListData = [];
+    currentWorkoutIndex = 0;
+    currentSet = 1;
+    clearInterval(timer); // Clear the interval timer
+    timerDisplay.textContent = '00:00'; // Reset the timer display
+    completionMessage.classList.add('d-none'); // Hide the completion message
+    document.getElementById('current-set').textContent = ''; // Clear the current set
+    currentExerciseDisplay.textContent = ''; // Clear the current exercise
+    startBtn.disabled = false; // Enable the start button
+    pauseBtn.disabled = true; // Disable the pause button
 }
 
 function startTimer() {
-  if (workoutListData.length > 0) {
-    startBtn.disabled = true;
-    pauseBtn.disabled = false;
-    completionMessage.classList.add('d-none'); // Hide the completion message
-    noWorkoutsWarning.classList.add('d-none'); // Hide the no workouts warning message
-    startWorkoutTimer();
-  } else {
-    noWorkoutsWarning.classList.remove('d-none');
-  }
+    if (workoutListData.length > 0) {
+        startBtn.disabled = true;
+        pauseBtn.disabled = false;
+        completionMessage.classList.add('d-none'); // Hide the completion message
+        noWorkoutsWarning.classList.add('d-none'); // Hide the no workouts warning message
+        startWorkoutTimer();
+    } else {
+        noWorkoutsWarning.classList.remove('d-none');
+    }
 }
 
 function pauseTimer() {
-  startBtn.disabled = false;
-  pauseBtn.disabled = true;
-  clearInterval(timer);
+    startBtn.disabled = false;
+    pauseBtn.disabled = true;
+    clearInterval(timer);
 }
 
 function startWorkoutTimer() {
-  let currentWorkoutIndex = 0;
-  let currentSet = 1;
-  let currentTime = workoutListData[currentWorkoutIndex].interval;
-  let isRestPeriod = false;
+    let currentWorkoutIndex = 0;
+    let currentSet = 1;
+    let currentTime = workoutListData[currentWorkoutIndex].interval;
+    let isRestPeriod = false;
 
-  // Get the DOM elements for workout state and current exercise
-  const workoutStateDisplay = document.getElementById('workout-state');
-  const currentExercise = workoutList.children[currentWorkoutIndex];
+    // Get the DOM elements for workout state and current exercise
+    const workoutStateDisplay = document.getElementById('workout-state');
+    const currentExercise = workoutList.children[currentWorkoutIndex];
 
-  // Highlight the current exercise and display the initial workout state
-  currentExercise.classList.add('active');
-  currentExerciseDisplay.textContent = `Current Exercise: ${workoutListData[currentWorkoutIndex].exerciseName}`;
-  speak('Workout')
-  workoutStateDisplay.textContent = 'Workout';
+    // Highlight the current exercise and display the initial workout state
+    currentExercise.classList.add('active');
+    currentExerciseDisplay.textContent = `Current Exercise: ${workoutListData[currentWorkoutIndex].exerciseName}`;
+    speak('Workout')
+    workoutStateDisplay.textContent = 'Workout';
 
-  timer = setInterval(() => {
-    currentTime--;
+    timer = setInterval(() => {
+        currentTime--;
+        if (currentTime < 0) {
+            nextStep();
+        } else {
+            timerDisplay.textContent = formatTime(currentTime);
+            document.getElementById('current-set').textContent = `Current Set: ${currentSet}`;
+            currentExerciseDisplay.textContent = `Current Exercise: ${workoutListData[currentWorkoutIndex].exerciseName}`;
+        }
+    }, 1000);
 
-    if (currentTime < 0) {
-      nextStep();
-    } else {
-      timerDisplay.textContent = formatTime(currentTime);
-      document.getElementById('current-set').textContent = `Current Set: ${currentSet}`;
-      currentExerciseDisplay.textContent = `Current Exercise: ${workoutListData[currentWorkoutIndex].exerciseName}`;
-    }
-  }, 1000);
+    function nextStep() {
+        isRestPeriod = !isRestPeriod;
 
-  function nextStep() {
-    isRestPeriod = !isRestPeriod;
+        if (isRestPeriod) {
+            speak('Rest')
+            workoutStateDisplay.textContent = 'Rest';
 
-    if (isRestPeriod) {
-      speak('Rest')
-      workoutStateDisplay.textContent = 'Rest';
+            if (currentSet < workoutListData[currentWorkoutIndex].sets) {
+                currentSet++;
+            } else {
+                currentSet = 1;
+                currentExercise.classList.remove('active');
+                currentExercise.classList.add('completed');
+                currentExercise.dataset.completed = true;
 
-      if (currentSet < workoutListData[currentWorkoutIndex].sets) {
-        currentSet++;
-      } else {
-        currentSet = 1;
-        currentExercise.classList.remove('active');
-        currentExercise.classList.add('completed');
-        currentExercise.dataset.completed = true;
+                currentWorkoutIndex++;
 
-        currentWorkoutIndex++;
+                if (currentWorkoutIndex >= workoutListData.length) {
+                    clearInterval(timer);
+                    startBtn.disabled = false;
+                    pauseBtn.disabled = true;
+                    timerDisplay.textContent = '00:00';
+                    document.getElementById('current-set').textContent = '';
+                    completionMessage.classList.remove('d-none'); // Show the completion message
+                    speak('All workouts completed.'); // Add text-to-speech for workout completion
+                    return;
+                }
 
-        if (currentWorkoutIndex >= workoutListData.length) {
-          clearInterval(timer);
-          startBtn.disabled = false;
-          pauseBtn.disabled = true;
-          timerDisplay.textContent = '00:00';
-          document.getElementById('current-set').textContent = '';
-          completionMessage.classList.remove('d-none'); // Show the completion message
-          speak('All workouts completed.'); // Add text-to-speech for workout completion
-          return;
+                // Highlight the next exercise
+                const nextExercise = workoutList.children[currentWorkoutIndex];
+                nextExercise.classList.add('active');
+            }
+            currentTime = workoutListData[currentWorkoutIndex].rest;
+        } else {
+            speak("Workout")
+            workoutStateDisplay.textContent = 'Workout';
+            currentTime = workoutListData[currentWorkoutIndex].interval;
         }
 
-        // Highlight the next exercise
-        const nextExercise = workoutList.children[currentWorkoutIndex];
-        nextExercise.classList.add('active');
-      }
-      currentTime = workoutListData[currentWorkoutIndex].rest;
-    } else {
-      speak("Workout")
-      workoutStateDisplay.textContent = 'Workout';
-      currentTime = workoutListData[currentWorkoutIndex].interval;
+        timerDisplay.textContent = formatTime(currentTime);
+        document.getElementById('current-set').textContent = `Current Set: ${currentSet}`;
+        currentExerciseDisplay.textContent = `Current Exercise: ${workoutListData[currentWorkoutIndex].exerciseName}`;
     }
+}
 
-    timerDisplay.textContent = formatTime(currentTime);
-    document.getElementById('current-set').textContent = `Current Set: ${currentSet}`;
-    currentExerciseDisplay.textContent = `Current Exercise: ${workoutListData[currentWorkoutIndex].exerciseName}`;
-  }
-
-  function formatTime(seconds) {
+function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
     return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
-  }
 }
-
-
-
 
 function speak(text) {
-  const synth = window.speechSynthesis;
-  const utterance = new SpeechSynthesisUtterance(text);
-  synth.speak(utterance);
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    synth.speak(utterance);
 }
-
-
